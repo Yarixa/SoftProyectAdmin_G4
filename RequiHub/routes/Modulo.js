1d46e86575d5a3165b3400b3f2602c24572985d3
miguel <jorquera1401@gmail.com>
2020-05-24 00:13:52 -0400

    Registro de modulo en la BD
    Issues: #6
    Se registra el modulo en la base de datos.
    Falta: Integrar con el Front
    Importante: Se utiliza metodo post, usando como parametros el cuerpo(body) de json para los valores de los atributos.

--- RequiHub/routes/Modulo.js
+++ RequiHub/routes/Modulo.js
@@ -7,15 +7,18 @@ const Modulo = require("../models/Modulo")
 
 modulos.use(cors())
 
+/*  Funcion para crear un modulo
+    Para los valores de los parametros se requiere del cuerpo(body) como parametro
+*/
 modulos.post('/register', (req,res)=>{
     const moduloData = {
-        id : req.id,
-        nombre : req.nombre,
-        degree : req.degree
+        id    : req.body.id,
+        nombre : req.body.nombre,
+        degree : req.body.degree
     }
     Modulo.findOne({
         where : {
-            id : req.id
+            id : req.body.id
         }
     }).then(modulo=>{
         if(!modulo){
@@ -29,13 +32,15 @@ modulos.post('/register', (req,res)=>{
                 res.send('error en crear '+err)
             })
         }else{
-            res.json({error:"modulo ya existe!"})
+            res.json({error:"Ya existe un modulo con este codigo"})
         }
     }).catch(err=>{
-        res.send('error: '+err)
+        res.send(`error al crear: ${err}`)
     })
 });
-
+/**
+ * Se utiliza el metodo get para traer  los modulos creados
+ */
 modulos.get('/show', (req,res)=>{
     Modulo.findAll().then(modulos=>{
         res.json({
