2eb41a7d2ffa19b6242d3e0337dfa044f309473a
Francisco <fco.alvarez.08@gmail.com>
2020-06-17 15:03:56 -0400

    Gestion Miembros Grupos
    Issues: #41
    
    SOLO estructura base del funcionamiento.

---
+++ frontend/src/components/grupo-curso/group-members/GroupMember.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Semantic Table | Estilos
+import {Table} from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+export default function GroupMember(props) {
+    
+    const {group} = props;
+
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ frontend/src/components/grupo-curso/group-members/boton-miembros/AgregarMiembro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Material Estilos
+import Button from '@material-ui/core/Button';
+
+export default function AgregarGrupo(props) {
+
+    return (
+        <Button>
+            Agregar Miembro
+        </Button>
+    )
+}
