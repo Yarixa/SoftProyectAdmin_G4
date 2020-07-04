0f7e9d148d7ddb5da02a571f99b8f10f511926da
Francisco <fco.alvarez.08@gmail.com>
2020-07-04 19:00:49 -0400

    Hotfix MassiveAdd Frontend
    Issue: #52
    Contiene:
     - Se arregló el problema de actualización del componente padre.
     - Se dividió el proceso en 2 click (botones)
    
    Problemas encontrados:
     - Al cargar y luego subir, el explorador de archivos se vuelve a abrir.
     - Al cargar un archivo con el mismo nombre de otro ya cargado, no se actualiza la lista de usuarios.

--- frontend/src/components/massive-add/MassiveAdd.jsx
+++ frontend/src/components/massive-add/MassiveAdd.jsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import {useDispatch} from 'react-redux'
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 
 import Button from '@material-ui/core/Button';
 import { uploadFile, loadFile } from './massiveAddDucks';
+import { getUsers } from '../gestion-usuarios/userDucks';
 
-export default function MassiveAdd () {
-
-    //const fileName = useSelector(store => store.massive.name);
+export default function MassiveAdd (props) {
+    
+    const {esVincular} = props;
+    const {idCurso} = props;
+    const {carga} = props;
     const dispatch = useDispatch();
+    const [fileName, setFileName] = React.useState('');
 
     const handleFile = e => {
-        console.log(e.target.files[0].name)
-        dispatch(uploadFile(e.target.files[0]))
-        dispatch(loadFile(e.target.files[0].name))
+        if (esVincular){
+
+        }
+        else{
+            setFileName(e.target.files[0].name)
+            dispatch(uploadFile(e.target.files[0]));
+            props.action(true);
+        }
     }
 
+    const handleLoad = e => {
+        if (esVincular){
+
+        }
+        else{
+            dispatch(loadFile(fileName));
+            setFileName('');
+            dispatch(getUsers());
+            props.action(false);
+        }
+    }
+    if (carga){
+        return (
+            <Button
+                variant="contained"
+                component="label"
+                color="secondary"
+                onClick={handleLoad}
+            >Subir Archivo</Button>
+        )
+    }
     return (
         <Button
-        variant="contained"
-        component="label"
+            variant="contained"
+            component="label"
         >
-        Cargar XLXS
+        Cargar Archivo
         <input
             type="file"
             style={{ display: "none" }}
