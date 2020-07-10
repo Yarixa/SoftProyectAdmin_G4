f53c735f69332c18b5006bb72313662b97d6f07c
Francisco <fco.alvarez.08@gmail.com>
2020-07-10 15:13:52 -0400

    Auto stash before merge of "francisco-release" and "origin/matias"

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize")
 const db = {}
 const sequelize = new Sequelize("firstApp", "administrador",
 	"abnormalize", {
-	host: 'firstapp.catt7lxq8hdv.us-east-2.rds.amazonaws.com',
+	host: '192.168.0.6',
 	dialect: 'mysql',
 	operatorAliases: false,
 
