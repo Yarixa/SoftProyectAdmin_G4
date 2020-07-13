715ab85be6e687687723b63e9ead88da931e2487
yarixa <ygalvez15@alumnos.utalca.cl>
2020-07-12 23:20:39 -0400

    #84 modificación componentes dashboard cursos

--- frontend/src/components/vista-modulos/visualizacion-curso/VistaCurso.js
+++ frontend/src/components/vista-modulos/visualizacion-curso/VistaCurso.js
@@ -54,56 +54,56 @@ export default function VistaCurso() {
             <Grid container spacing={4}>
                 <Card className={classes.root}>
                     <CardContent>
-                        <BotonAgregar></BotonAgregar>
+                        <BotonAgregar></BotonAgregar>{/* Agregar acción de redirección */}
                         <Typography className={classes.title} color="textSecondary" gutterBottom>Grupos</Typography>
                         <Typography className={classes.pos} color="textSecondary" variant="h3" component="h2">6</Typography>
                     </CardContent>
                     <CardActions>
-                        <BotonGestionar></BotonGestionar>
+                        <BotonGestionar></BotonGestionar>{/* Agregar acción de redirección */}
                     </CardActions>
                 </Card>
 
 
                 <Card className={classes.root}>
                     <CardContent>
-                        <BotonAgregar></BotonAgregar>
+                        <BotonAgregar></BotonAgregar>{/* Agregar acción de redirección */}
                         <Typography className={classes.title} color="textSecondary" gutterBottom>Proyectos</Typography>
                         <Typography className={classes.pos} color="textSecondary" variant="h3" component="h2">2</Typography>
                     </CardContent>
                     <CardActions>
-                        <BotonGestionar></BotonGestionar>
+                        <BotonGestionar></BotonGestionar>{/* Agregar acción de redirección */}
                     </CardActions>
                 </Card>
 
 
                 <Card className={classes.root}>
                     <CardContent>
-                        <BotonAgregar></BotonAgregar>
+                        <BotonAgregar></BotonAgregar>{/* Agregar acción de redirección */}
                         <Typography className={classes.title} color="textSecondary" gutterBottom>Integrantes</Typography>
                         <Typography className={classes.pos} color="textSecondary" variant="h3" component="h2">28</Typography>
                     </CardContent>
                     <CardActions>
-                        <BotonGestionar></BotonGestionar>
+                        <BotonGestionar></BotonGestionar>{/* Agregar acción de redirección */}
                     </CardActions>
                 </Card>
 
                 <Card className={classes.root}>
                     <CardContent>
-                        <BotonAgregar></BotonAgregar>
+                        <BotonAgregar></BotonAgregar>{/* Agregar acción de redirección */}
                         <Typography className={classes.title} color="textSecondary" gutterBottom>Ayudante
                         </Typography>
                         <Typography className={classes.pos}  component="h2">Andres Andrade Norambuena</Typography>
                         <Typography className={classes.pos} color="textSecondary" component="h2">Ayudantías Lunes Bloque 2 y 3</Typography>
                     </CardContent>
                     <CardActions>
-                        <BotonGestionar></BotonGestionar>
+                        <BotonGestionar></BotonGestionar>{/* Agregar acción de redirección */}
                     </CardActions>
                 </Card>
 
 
                 <Card className={classes.tarjetaGrupo}>
                     <CardContent>
-                        <Typography className={classes.title} color="textSecondary" gutterBottom>Resumen Curso</Typography>
+                        <Typography className={classes.title} color="textSecondary" gutterBottom>Resumen Grupos</Typography>
                         <TablaGrupos></TablaGrupos>
                     </CardContent>
                 </Card>
