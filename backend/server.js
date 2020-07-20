dd085009b73bcc4eb029a1f6f6fc3738992148ab
Ricardo <ricardonavarro354@gmail.com>
2020-07-19 23:30:20 -0400

    Refinamiento mongoDB
    -Se cambió la librería para utilizar mongoDB (de mongoose se pasó a mongodb), además se configuraron las rutas respectivas, las cuales se especifican en el documento de rutas de back-end
    -En una futura mejor se espera separar las rutas de mongoDB de server.js

--- backend/server.js
+++ backend/server.js
@@ -20,28 +20,82 @@ var Modulos = require("./routes/Modulo")
 var Courses = require("./routes/Courses")
 var MemberList = require("./routes/MemberList")
 var Projects = require("./routes/Project")
-var Documents = require("./routes/Documents")
 
 app.use("/courses", Courses)
 app.use("/modulos", Modulos)
 app.use("/users", Users)
 app.use("/memberlist", MemberList)
 app.use("/projects", Projects)
-app.use("/documents", Documents)
 
-//Conexión con mongoDB
-const db = require("./models/mongoDB");
-db.mongoose.connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Conectado a mongoDB");
-  })
-  .catch(err => {
-    console.log("No se puedo conectar a mongoDB", err);
-    process.exit();
+//----------------Conexión mongoDB---------//
+
+const MongoClient = require("mongodb").MongoClient
+const ObjectId = require("mongodb").ObjectID
+const URL = "mongodb://localhost:27017/mongoDB";
+const NOMBREDATABASE = "mongoDB";
+var database, collection
+MongoClient.connect(URL,{ useUnifiedTopology: true }, { useNewUrlParser: true }, (error, client) => {
+  if(error) {
+      throw error;
+  }
+  database = client.db(NOMBREDATABASE);
+  collection = database.collection("documents");
+  console.log("Conectado a `" + NOMBREDATABASE + "`!");
+});
+
+//---------Rutas mongoDB-----------------//
+//Agregar documento
+app.post("/documents/add", (request, response) => {
+  request.body.disponible = true //se agrega disponibilidad true por defecto
+  collection.insertOne(request.body, (error, result) => {
+    if(error) {
+      return response.status(500).send(error);
+    }
+    //response.send(request.id)
+    response.send(result["ops"][0]["_id"]);
   });
+});
+
+//Obtener todos los documentos
+app.get("/documents/readAll", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+//Obtener unico documento según su ID
+app.get("/documents/get/:id", (request, response) => {
+  collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
+  });
+});
+
+//Borrar permamentemente documento según su ID
+app.delete("/documents/delete/:id", (request, response) => {
+  collection.deleteOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
+  });
+});
+
+//Borrar permamentemente todos los documentos ¡¡¡PRECAUCIÓN!!!
+app.delete("/documents/deleteAll/", (request, response) => {
+  collection.deleteMany((error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
+  });
+});
+//--------------Final rutas mongoDB--------//
 
 app.listen(port, () => {
 	console.log("Server is running on port: " + port)
