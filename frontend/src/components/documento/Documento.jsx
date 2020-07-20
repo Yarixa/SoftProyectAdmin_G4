b3a209a88eb5e76ec0d2027f83822dde75ec4d18
miguel <jorquera1401@gmail.com>
2020-07-19 22:42:26 -0400

    Agregar Nuevas funcionalidades Documento
    Se agregar nuevos elementos en la creacion de documentos
    #Issues: #77
    Falta: Testear con back

--- frontend/src/components/documento/Documento.jsx
+++ frontend/src/components/documento/Documento.jsx
@@ -3,56 +3,87 @@ import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { makeStyles, CardHeader } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
-
 import Button from "@material-ui/core/Button";
 import Card from '@material-ui/core/Card'
 import CardContent  from "@material-ui/core/CardContent";
 import { createDocument, disableDocumento, enableDocumento, editarDocumento, getDocument} from './documentDucks';
 
-
 export default function Documentos(){
     const documento = "hola "
     const nombreProyecto ="Proyecto de Requisitos de Software";
     const curso = "curso1"
+    const requisito_software=""
+    const requisito_usuario = ""
+    /**
+     * 
+     * @param {text} contenido 
+     * contenido del texto enriquecido 
+     * 
+     */
     const guardarDato =(contenido)=>{
         console.log('hizo clic',contenido)
     }
-    
 
+    const agregarRequisitoSoftware=(contenido)=>{
+        return contenido
+    }
+    const agregarRequisitoUsuario=(contenido)=>{
+        return contenido
+    }
+    const cargarRequisitoSoftware=(contenido){
+        requisito_software=contenido
+    }
+    const cargarRequisitoUsuario=(contenido){
+        requisito_usuario=contenido
+    }
     return (
-
         <div>
             <Card>
                 <CardHeader title={nombreProyecto}/>
                 <CardContent>
                     <h3>{curso}</h3>
                 </CardContent>
-           
                 <CKEditor 
                     editor = {ClassicEditor}
                     data = "<p>Ingresar Dato</p>"
                     onChange = {(event, editor)=>{
                     }}
-                    config ={{
-                       
+                    config ={{   
                         autosave:{
                             guardar(editor){
                                 console.log("Guardado")
                                 return guardarDato(editor.getData())
                             }
                         }
-                    }}
-
-
-
-                    
-                    
+                    }}       
                 />
                   <Button type="submit">Agregar</Button>
             </Card>   
-           
-        </div>
-       
 
+        </div>
+        <div>
+        <Card>
+            <CardHeader title={nombreProyecto}/>
+            <CardContent>
+                <h3>Requisito de Software</h3>
+            </CardContent>
+            <CKEditor 
+                editor = {ClassicEditor}
+                data = "<p>Requisitos</p>"
+                onChange = {(event, editor)=>{
+                }}
+                config ={{   
+                    autosave:{
+                        guardar(editor){
+                            console.log("Guardado")
+                            return guardarDato(editor.getData())
+                        }
+                    }
+                }}       
+            />
+              <Button type="submit">Agregar</Button>
+        </Card>   
+        
+    </div>
     );
 }
