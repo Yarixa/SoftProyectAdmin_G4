803dabcd67d2b38bea8effc1dca2220a0b8dd000
Francisco <fco.alvarez.08@gmail.com>
2020-07-26 19:47:41 -0400

    Seleccion dinamica (Avance)
    Issue: #96
    
    Contiene:
     - Se conecta funcionalidad de la tabla con los Reducers.
     - Se implementa fetch de usuarios dinamicamente.
     - Se preparan las rutas con el back.

--- frontend/src/components/grupo-curso/selectMemberDucks.js
+++ frontend/src/components/grupo-curso/selectMemberDucks.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Constantes
 const dataInicial = {
-    members: []
+    members: [],
+    seleccionados: []
 }
 
 const apiURL = process.env.REACT_APP_API_URL;
@@ -10,14 +11,17 @@ const apiURL = process.env.REACT_APP_API_URL;
 // Tipos
 const SELECT_MEMBER = "SELECT_MEMBER";
 const UNSELECT_MEMBER = "UNSELECT_MEMBER";
+const FETCH_LIST = "FETCH_LIST";
 
 // Reducers
 export default function selectMemberReducers(state = dataInicial, action){
     switch(action.type){
         case SELECT_MEMBER:
-            return {...state, members: state.members.concat(action.payload)}
+            return {...state, seleccionados: state.seleccionados.concat(action.payload)}
         case UNSELECT_MEMBER:
-            return {...state, members: state.members.filter(member => member.email != action.payload.embail)}
+            return {...state, seleccionados: state.seleccionados.filter(member => member.email != action.payload.embail)}
+        case FETCH_LIST:
+            return {...state, members: action.payload}
         default:
             return state
     }
@@ -38,7 +42,37 @@ export const unselectIntegrante = (integrante) => dispatch => {
     })
 }
 
-export const getMembers = () => dispatch => {
+export const getTeamMembers = () => dispatch => {
     //BACK
-    dispatch({})
+    console.log("A")
+    const list = [{
+        id: "1",
+        first_name: "Francisco Javier",
+        last_name: "Alvarez Aspee",
+        email: "falvarez16@alumnos.utalca.cl"
+    }]
+    //Agregar parámetro de selección
+    const aux_list = list.map(member => ({...member, selected: false}))
+    dispatch({
+        type: FETCH_LIST,
+        payload: aux_list
+    })
+}
+
+export const getCourseMembers = () => dispatch => {
+    console.log("B")
+    //BACK
+    //Agregar parámetro de selección
+    dispatch({
+        type: FETCH_LIST
+    })
+}
+
+export const getProfesors = () => dispatch => {
+    console.log("C")
+    //BACK
+    //Agregar parámetro de selección
+    dispatch({
+        type: FETCH_LIST
+    })
 }
