746aa7827a578bb5c475dd034a591a1bfe905de1
Ricardo Navarro <ricardonavarro354@gmail.com>
2020-07-26 22:42:50 -0400

    Refinamiento y creacion de rutas de documentos en backend
    Issues: #99
    -Se creó un archivo en /routes/ el cual contiene todo lo necesario respecto a la conexion con mongodb y las rutas de documentos, ya que estos estaban dispersos en el archivo server.js
    -Se crearon rutas extras para la obtención de documentos

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,3 @@
-//------Variables mongoDB-------//
-const MongoClient = require("mongodb").MongoClient
-const ObjectId = require("mongodb").ObjectID
-const mongoDBURL = "mongodb://localhost:27017"
-const mongoDBDatabaseName = "mongoDB"
-var mongoDBdatabase, mongoDBcollection
-//------------------------------//
-
 var express = require("express")
 var cors = require("cors")
 var bodyParser = require("body-parser")
@@ -28,105 +20,14 @@ var Modulos = require("./routes/Modulo")
 var Courses = require("./routes/Courses")
 var MemberList = require("./routes/MemberList")
 var Projects = require("./routes/Project")
+var Documents = require("./routes/Documents")
 
 app.use("/courses", Courses)
 app.use("/modulos", Modulos)
 app.use("/users", Users)
 app.use("/memberlist", MemberList)
 app.use("/projects", Projects)
-
-//----------------Conexión mongoDB---------//
-MongoClient.connect(mongoDBURL,{ useUnifiedTopology: true }, { useNewUrlParser: true }, (error, client) => {
-  if(error) {
-      throw error
-  }
-  mongoDBdatabase = client.db(mongoDBDatabaseName)
-  mongoDBcollection = mongoDBdatabase.collection("documents")
-  console.log("Conectado a `" + mongoDBDatabaseName + "`!")
-});
-//----------------Rutas mongoDB-----------------//
-//Agregar documento
-app.post("/documents/add", (request, response) => {
-  request.body.disponible = true //se agrega disponibilidad true por defecto
-  mongoDBcollection.insertOne(request.body, (error, result) => {
-    if(error) {
-      return response.status(500).send(error)
-    }
-    //response.send(request.id)
-    response.send(result["ops"][0]["_id"])
-  });
-});
-
-//Obtener todos los documentos
-app.get("/documents/readAll", (request, response) => {
-    mongoDBcollection.find({}).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error)
-        }
-        response.send(result)
-    });
-});
-
-//Obtener unico documento según su ID
-app.get("/documents/get/:id", (request, response) => {
-  mongoDBcollection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
-      if(error) {
-          return response.status(500).send(error)
-      }
-      response.send(result)
-  });
-});
-
-//Deshabilitar dococumento según su ID
-app.put("/documents/deshabilitar/:id", (request, response) => {
-  mongoDBcollection.updateOne({ "_id": new ObjectId(request.params.id) },  { $set: {disponible: false} },(error, result) => {
-      if(error) {
-          return response.status(500).send(error)
-      }
-      response.send(result)
-  });
-});
-
-//Habilitar dococumento según su ID
-app.put("/documents/habilitar/:id", (request, response) => {
-  mongoDBcollection.updateOne({ "_id": new ObjectId(request.params.id) },  { $set: {disponible: true} },(error, result) => {
-      if(error) {
-          return response.status(500).send(error)
-      }
-      response.send(result)
-  });
-});
-
-//Editar dococumento según su ID
-app.put("/documents/update/:id", (request, response) => {
-  mongoDBcollection.updateOne({ "_id": new ObjectId(request.params.id) },  { $set: request.body },(error, result) => {
-      if(error) {
-          return response.status(500).send(error)
-      }
-      response.send(result)
-  });
-});
-
-//Borrar permamentemente documento según su ID
-app.delete("/documents/delete/:id", (request, response) => {
-  mongoDBcollection.deleteOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
-      if(error) {
-          return response.status(500).send(error)
-      }
-      response.send(result)
-  });
-});
-
-//Borrar permamentemente todos los documentos ¡¡¡PRECAUCIÓN!!!
-app.delete("/documents/deleteAll/", (request, response) => {
-  mongoDBcollection.deleteMany((error, result) => {
-      if(error) {
-          return response.status(500).send(error)
-      }
-      response.send(result)
-  });
-});
-//--------------Final rutas mongoDB--------//
+app.use("/documents", Documents)
 
 app.listen(port, () => {
 	console.log("Server is running on port: " + port)
