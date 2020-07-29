e240ea75f282495016df59ef0a306e01fbf73919
Ricardo <ricardonavarro354@gmail.com>
2020-07-29 17:37:57 -0400

    Template documento de requisitos
    Issues: #103
    -Se termina la versión preliminar del template para el documento de requisitos

--- backend/documents/requisitos/template.js
+++ backend/documents/requisitos/template.js
@@ -1,15 +1,121 @@
 module.exports = ({ name, price1, price2, receiptId }) => {
     const today = new Date();
 return `
-<div>&nbsp;</div>
-<h4 style="text-align: left;"><strong>UNIVERSIDAD DE TALCA</strong><br /><strong>FACULTAD DE INGENIER&Iacute;A</strong></h4>
-<h4 style="text-align: left;">DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION (DCC)</h4>
-<div style="text-align: left;">&nbsp;</div>
-<div style="text-align: left;">&nbsp;</div>
-<div style="text-align: center;">
-<h1><strong>Documento de Especificaci&oacute;n de Requisitos de Usuario/Software</strong></h1>
-<h1><strong>Nombre de Proyecto</strong></h1>
-</div>
-<div>&nbsp;</div>
-    `;
+<p style="text-align: left;"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Utalca.svg/300px-Utalca.svg.png" width="70" height="70" /><strong><br /></strong><strong>FACULTAD DE INGENIER&Iacute;A<br /></strong><strong>UNIVERSIDAD DE TALCA<br /></strong>DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION (DCC)&nbsp;&nbsp;</p>
+<div style="text-align: center;">&nbsp;</div>
+<div style="text-align: center;">&nbsp;</div>
+<div style="text-align: left;">
+<h1>&nbsp;</h1>
+<h1>&nbsp;</h1>
+<h1>&nbsp;</h1>
+<h1 style="text-align: center;"><strong>Documento de Especificaci&oacute;n de Requisitos de Usuario/Software</strong></h1>
+<h1 style="text-align: center;"><strong>Nombre de Proyecto</strong></h1>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p style="text-align: right;"><strong>Fecha: ${`${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`}</strong></p>
+<p style="text-align: right;"><strong>Versi&oacute;n:</strong></p>
+<br />
+<div style="page-break-after: always;">&nbsp;</div>
+<p style="text-align: left;">&nbsp;</p>
+<p style="text-align: left;">&nbsp;</p>
+<p style="text-align: left;">&nbsp;</p>
+<p style="text-align: left;">&nbsp;</p>
+<p style="text-align: left;"><strong>&nbsp; &nbsp; Equipo de Desarrollo:</strong></p>
+<table style="height: 37px; width: 605px; border: 1px solid black; border-collapse: collapse; margin-left: auto; margin-right: auto;">
+<tbody>
+<tr>
+<td style="width: 214px; border: 1px solid black; border-collapse: collapse;"><strong>Nombre</strong></td>
+<td style="width: 140px; border: 1px solid black; border-collapse: collapse;"><strong>Rol</strong></td>
+<td style="width: 229px; border: 1px solid black; border-collapse: collapse;"><strong>Contacto</strong></td>
+</tr>
+<tr>
+<td style="width: 214px; border: 1px solid black; border-collapse: collapse;">test</td>
+<td style="width: 140px; border: 1px solid black; border-collapse: collapse;">test</td>
+<td style="width: 229px; border: 1px solid black; border-collapse: collapse;">test</td>
+</tr>
+</tbody>
+</table>
+<br />&nbsp; &nbsp;&nbsp;<strong>Contraparte:</strong></div>
+<div style="text-align: left;"><strong>&nbsp;&nbsp;</strong></div>
+<table style="height: 37px; width: 605px; border: 1px solid black; border-collapse: collapse; margin-left: auto; margin-right: auto;">
+<tbody>
+<tr>
+<td style="width: 214px; border: 1px solid black; border-collapse: collapse;"><strong>Nombre</strong></td>
+<td style="width: 140px; border: 1px solid black; border-collapse: collapse;"><strong>Rol</strong></td>
+<td style="width: 229px; border: 1px solid black; border-collapse: collapse;"><strong>Contacto</strong></td>
+</tr>
+<tr>
+<td style="width: 214px; border: 1px solid black; border-collapse: collapse;">test</td>
+<td style="width: 140px; border: 1px solid black; border-collapse: collapse;">test</td>
+<td style="width: 229px; border: 1px solid black; border-collapse: collapse;">test</td>
+</tr>
+</tbody>
+</table>
+<div style="page-break-after: always;">&nbsp;</div>
+<h1 style="text-align: center;">&nbsp;</h1>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<h1 style="text-align: center;">&Iacute;ndice</h1>
+<p>&nbsp;</p>
+<p style="text-align: center;"><strong>1&nbsp;&nbsp;&nbsp; Introducci&oacute;n............................................................................................................................. 1</strong></p>
+<p style="text-align: center;">1.1&nbsp;&nbsp;&nbsp; Prop&oacute;sito del Sistema................................................................................................................ 1</p>
+<p style="text-align: center;">1.2&nbsp;&nbsp;&nbsp; Alcance del Proyecto................................................................................................................. 1</p>
+<p style="text-align: center;">1.3&nbsp;&nbsp;&nbsp; Contexto................................................................................................................................... 1</p>
+<p style="text-align: center;">1.4&nbsp;&nbsp;&nbsp; Referencias............................................................................................................................... 1</p>
+<p style="text-align: center;"><strong>2&nbsp;&nbsp;&nbsp; Descripci&oacute;n General............................................................................................................... 2</strong></p>
+<p style="text-align: center;">2.1&nbsp;&nbsp;&nbsp; Suposiciones y Dependencias..................................................................................................... 2</p>
+<p style="text-align: center;">2.2&nbsp;&nbsp;&nbsp; Restricciones Generales............................................................................................................. 2</p>
+<p style="text-align: center;"><strong>3&nbsp;&nbsp;&nbsp; Requisitos del Usuario........................................................................................................... 3</strong></p>
+<div style="page-break-after: always;">&nbsp;</div>
+<p>&nbsp;</p>
+<h1><a name="_Toc353436287"></a>&nbsp; 1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Introducci&oacute;n</h1>
+<h2><a name="_Toc353436288"></a>&nbsp; 1.1&nbsp;&nbsp;&nbsp; Prop&oacute;sito del Sistema</h2>
+<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp;</p>
+<h2><a name="_Toc353436288"></a>&nbsp; 1.2&nbsp;&nbsp;&nbsp; Alcance del Proyecto</h2>
+<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp;</p>
+<h2><a name="_Toc353436288"></a>&nbsp; 1.3&nbsp;&nbsp;&nbsp; Contexto</h2>
+<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp;</p>
+<h2><a name="_Toc353436288"></a>&nbsp; 1.4&nbsp;&nbsp;&nbsp; Referencias</h2>
+<p>&nbsp; &nbsp;${name}</p>
+<div style="page-break-after: always;">&nbsp;</div>
+<p>&nbsp;</p>
+<h1><a name="_Toc353436287"></a>&nbsp; 2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Descripci&oacute;n general</h1>
+<h2><a name="_Toc353436288"></a>&nbsp; 2.1&nbsp;&nbsp;&nbsp; Suposiciones y Dependencias</h2>
+<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp;</p>
+<h2><a name="_Toc353436288"></a>&nbsp; 2.2&nbsp;&nbsp;&nbsp; Restricciones Generales</h2>
+<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp;</p>
+<h2><a name="_Toc353436288"></a>&nbsp; 2.3&nbsp;&nbsp;&nbsp; Usuarios</h2>
+<p>&nbsp; &nbsp;${name}</p>
+<div style="page-break-after: always;">&nbsp;</div>
+<p>&nbsp;</p>
+<h1><a name="_Toc353436287"></a>&nbsp; 3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Requisitos de Usuario</h1>
+<p>&nbsp;</p>
+<table style="margin-left: auto; margin-right: auto; border: 1px solid black; border-collapse: collapse; height: 206px;" width="529">
+<tbody>
+<tr>
+<td style="width: 525px; text-align: left;">&nbsp;<strong style="text-align: left;">RU0002</strong><strong style="text-align: left;"> - Validaci&oacute;n de los alumnos</strong>
+<p style="text-align: left;"><strong>Descripci&oacute;n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :</strong> Validaci&oacute;n del nombre de usuario, contrase&ntilde;a y carrera al momento de autentificarse en el sistema.</p>
+<p style="text-align: left;"><strong>Fuente&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : </strong>Cliente</p>
+<p style="text-align: left;"><strong>Estabilidad&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Intransable/intransable</strong></p>
+<p style="text-align: left;"><strong>Tipo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Funcional/restricci&oacute;n/calidad</strong></p>
+</td>
+</tr>
+</tbody>
+</table>
+`;
 };
