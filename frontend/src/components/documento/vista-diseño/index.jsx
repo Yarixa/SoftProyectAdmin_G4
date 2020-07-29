5a45c3a7cc5d8493f1add7f97e3c703a2962187d
miguel <jorquera1401@gmail.com>
2020-07-29 16:46:57 -0400

    Se rediseña la GUI para gestion de diseño
    Se agrega la funcionalidad de cargar imagenes para los distintos diagrama que se requiera
    Issues #98
    Falta: Guardar en BD, agregar elementos de creacion

--- frontend/src/components/documento/vista-diseño/index.jsx
+++ frontend/src/components/documento/vista-diseño/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import { makeStyles, CardHeader, Container, Grid } from '@material-ui/core';
+import { CardHeader, Container, Grid } from '@material-ui/core';
+import {makeStyles} from "@material-ui/core/styles";
 import { useDispatch, useSelector } from 'react-redux';
 import Button from "@material-ui/core/Button";
 import Card from '@material-ui/core/Card'
@@ -9,6 +10,7 @@ import CardContent  from "@material-ui/core/CardContent";
 import IconButton from "@material-ui/core/IconButton";
 import SaveIcon from "@material-ui/icons/Save";
 import Paper from "@material-ui/core/Paper";
+import Modal from "@material-ui/core/Modal";
 
 const estilo = makeStyles((theme)=>({
     root :{
@@ -20,57 +22,103 @@ const estilo = makeStyles((theme)=>({
     },
 }));
 
+function getModalStyle(){
+    const top = 50;
+    const left = 50 ;
+
+    return {
+        top : '${top}%',
+        left: '${left}%',
+        transform : 'translate(-${top}%,-${left}%)'
+    };
+}
+
+
 export default function VistaDisenio(props){
+
+        
+    //this.state = {image:null};
+   // this.onImageChange = this.onImageChange.bind(this);
+    const state = {image:null}
+
+
+    const onImageChange = event=>{
+        if(event.target.files && event.target.files[0]){
+            let img  = event.target.files[0];
+           state.image = img;
+        
+        }
+    };
+
+
+    
+    const [open, setOpen] = React.useState(false);
+
+    const [modalStyle]=React.useState(getModalStyle);
+
+    const handleOpen = ()=>{
+        setOpen(true);
+    }
+    const handleClose=()=>{
+        setOpen(false)
+    }
+
     const classes = estilo();
     const {idProyecto} = props;
+
+    
+    const body=(
+        <div style={modalStyle}>
+            <h2>Modal</h2>
+            <p>
+                Esto es una prueba de modal
+            </p>
+        </div>
+    );
 
     return (
 
         
         <div>
-            <Container fixed className={classes.paper}>
-            <Grid container spacing={3}>
+                 <Container fixed className={classes.paper}>
+            <Grid container spacing={9}>
                 <Grid item xs>
                     <Card>
                         <CardHeader title="Documento de Diseño"/>
                         
                     <CardContent>
-                        <h5>Deberia ir la imagen para visualizar</h5>
+                        <img src={state.image} />
                     </CardContent>
                     </Card>
                 </Grid>
-                <Grid item xs>
-                   
-                        <label htmlFor="container-button-file">
-                        <Button variant="contained" color="primary" component="span">
+                <Grid item xs={3} container direction="column" spacing={1}>
+                   <Grid item xs>
+                       <input type="file" nmae="Imagen" onChange={onImageChange}/>
+                        <Button variant="contained" color="primary" component="span" size="large" onClick={handleOpen}> 
                             Subir diseño 
                         </Button>
-                        </label>
-                        <input accept="image/*" id="icon-button-file" type="file"/>
-             
+                   </Grid>
+                   <Grid item xs>
+                   <Button variant="contained" color="primary" size="large" startIcon={<SaveIcon />}>
+                            Guardar Documento
+                        </Button>
+                   </Grid>
                 </Grid>
             </Grid>
             <Grid item xs>
                 <CKEditor 
                         editor = {ClassicEditor}
                         data = "<p>Ingresar Descripcion del documento</p>"
-                        
-    
                     />
             </Grid>
-
-            <Grid>
-                <Grid item xs={12}>
-                    <Button 
-                        variant="contained"
-                        color="primary"
-                        size="large"
-                        startIcon={<SaveIcon />}>
-                        Guardar Documento
-                    </Button>
-                </Grid>
-            </Grid>
         </Container>
+        
+        <Modal
+            open={open}
+            onClose={handleClose}
+            >
+            {body}
+        </Modal>
         </div>
     );
 }
