1c59dc5ff45db6dae4e47e3e6f070a7a5ff3c787
miguel <jorquera1401@gmail.com>
2020-07-30 03:09:16 -0400

    Carga de imagenes gestion de documento de diseño
    Se puede cargar imagenes a la vista de los diseños, tambien viene se puede aplicar zoom para los detalles.
    Issues #98
    Falta: Conectar con BD
    Importante: libreria para zoom de la imagen "react-zoom-pan-pinch" puede manejar la imagen con el mouse, se implementará los botones para mejor entendimiento con el usuario

--- frontend/src/components/documento/vista-diseño/index.jsx
+++ frontend/src/components/documento/vista-diseño/index.jsx
@@ -3,122 +3,146 @@ import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CardHeader, Container, Grid } from '@material-ui/core';
 import {makeStyles} from "@material-ui/core/styles";
-import { useDispatch, useSelector } from 'react-redux';
 import Button from "@material-ui/core/Button";
 import Card from '@material-ui/core/Card'
 import CardContent  from "@material-ui/core/CardContent";
 import IconButton from "@material-ui/core/IconButton";
 import SaveIcon from "@material-ui/icons/Save";
-import Paper from "@material-ui/core/Paper";
-import Modal from "@material-ui/core/Modal";
+import ImageIcon from "@material-ui/icons/Image";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import Snackbar from "@material-ui/core/Snackbar";
 
-const estilo = makeStyles((theme)=>({
-    root :{
-        flexGrow:1
+import {TransformComponent,TransformWrapper} from "react-zoom-pan-pinch";
+
+
+
+const useStyle = makeStyles((theme)=>({
+    root: {
+        maxWidth:"100%",
+        borderStyle:"groove"
     },
-    paper: {
-        padding:theme.spacing(2),
-        color:theme.palette.text.secondary
+    botones : {
+        position:"static",
+        textAlign:"center"
     },
-}));
+    margenes:{
+        marginBottom:"2%"
+    },
+    raiz : {
+        padding:"2%"
+    },
+    contenedorImg : {
+        height:"400px",
+        textAlign:"center"
+    },
+    imagen : {
+        height:"70%",
+        width:"70%",
+    }
 
-function getModalStyle(){
-    const top = 50;
-    const left = 50 ;
-
-    return {
-        top : '${top}%',
-        left: '${left}%',
-        transform : 'translate(-${top}%,-${left}%)'
-    };
-}
+    
+}))
 
 
 export default function VistaDisenio(props){
+    const classes = useStyle();
+    const [state,setState]=React.useState({
+        open:false,
+        vertical:'top',
+        horizontal:'right',
+    });
+    const {vertical,horizontal,open}=state;
+    const handleClick = (newState)=>()=>{
+        setState({open:true,...newState});
+    }
+    const handleClose =()=>{
+        setState({...state,open:false});
+    }
 
-        
-    //this.state = {image:null};
-   // this.onImageChange = this.onImageChange.bind(this);
-    const state = {image:null}
+    const titulo = "Proyecto de requisito"
+    const uploadedImage = React.useRef(null);
+    const imageUploader = React.useRef(null);
 
-
-    const onImageChange = event=>{
-        if(event.target.files && event.target.files[0]){
-            let img  = event.target.files[0];
-           state.image = img;
-        
+    const handleImageUpload = e => {
+        const [file] = e.target.files;
+        if(file){
+            const reader = new FileReader();
+            const {current} = uploadedImage;
+            current.file = file;
+            reader.onload = e =>{
+                current.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+            handleClick({vertical:'top', horizontal:'right'});
         }
     };
 
+    
 
     
-    const [open, setOpen] = React.useState(false);
-
-    const [modalStyle]=React.useState(getModalStyle);
-
-    const handleOpen = ()=>{
-        setOpen(true);
-    }
-    const handleClose=()=>{
-        setOpen(false)
-    }
-
-    const classes = estilo();
-    const {idProyecto} = props;
-
-    
-    const body=(
-        <div style={modalStyle}>
-            <h2>Modal</h2>
-            <p>
-                Esto es una prueba de modal
-            </p>
-        </div>
-    );
 
     return (
 
         
-        <div>
-                 <Container fixed className={classes.paper}>
-            <Grid container spacing={9}>
-                <Grid item xs>
-                    <Card>
-                        <CardHeader title="Documento de Diseño"/>
+        
+        <div className={classes.raiz}>
+            <Container fixed  className={classes.root}>
+                <Grid className={classes.margenes} container spacing={9}>
+                    <Grid item xs>
+                        <Card>
+                            <CardHeader title={titulo}>
+                            
+                            </CardHeader>
+                            <CardContent className={classes.contenedorImg}>
+                                <TransformWrapper>
+                               
+                                    <TransformComponent>
+                                    <img className={classes.imagen} 
+                                        ref={uploadedImage}
+                                        
+                                    />   
+                                    </TransformComponent>
+                                </TransformWrapper>
+                                
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid className={classes.menu} item xs={3} container direction="column" spacing={1}>
                         
-                    <CardContent>
-                        <img src={state.image} />
-                    </CardContent>
-                    </Card>
+                        <Grid item xs direction="column" spacing={2}>
+                            <div>
+                            <input type="file"  accept="image/*" style={{display:"none"}} onChange={handleImageUpload} ref={imageUploader} />
+                            <Button startIcon={<ImageIcon/>} variant="contained" color="primary" component="span"  onClick={()=>imageUploader.current.click()}> 
+                                Subir 
+                            </Button>
+                                                    
+                            <Button variant="contained" color="primary"  startIcon={<SaveIcon />}>
+                                Guardar
+                            </Button>
+                            </div>
+                        </Grid> 
+                    </Grid>
                 </Grid>
-                <Grid item xs={3} container direction="column" spacing={1}>
-                   <Grid item xs>
-                       <input type="file" nmae="Imagen" onChange={onImageChange}/>
-                        <Button variant="contained" color="primary" component="span" size="large" onClick={handleOpen}> 
-                            Subir diseño 
-                        </Button>
-                   </Grid>
-                   <Grid item xs>
-                   <Button variant="contained" color="primary" size="large" startIcon={<SaveIcon />}>
-                            Guardar Documento
-                        </Button>
-                   </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs>
+                <Grid className={classes.margenes} item xs>
+                <h3>Descipción</h3>
                 <CKEditor 
                         editor = {ClassicEditor}
                         data = "<p>Ingresar Descripcion del documento</p>"
                     />
-            </Grid>
-        </Container>
-        
-        <Modal
-            open={open}
-            onClose={handleClose}
-            >
-            {body}
-        </Modal>
+                </Grid>
+                
+            </Container>    
+            <Grid  className={classes.botones}>
+                    <Button  startIcon={<AddCircleIcon/>}  size="expand" variant="contained" color="primary" >Agregar Diseño</Button>
+                </Grid>   
+
+            <Snackbar 
+                anchorOrigin={{vertical,horizontal}}
+                open={open}
+                onClose={handleClose}
+                message="Se cargo la imagen Correctamente"
+                key={vertical+horizontal}
+            />
         </div>
     );
 }
