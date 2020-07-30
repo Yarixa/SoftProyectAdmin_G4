71de7cfe6fb0414c560f4439a2be9e9013ba9596
Ricardo Navarro <ricardonavarro354@gmail.com>
2020-07-30 00:59:48 -0400

    Refinamiento template requisitos
    Issues: #103, #99
    -Refinamiento a la generación de documento de requisitos.

--- backend/documents/requisitos/template.js
+++ backend/documents/requisitos/template.js
@@ -1,4 +1,23 @@
-module.exports = ({ name, price1, price2, receiptId }) => {
+module.exports = (result1, result2) => {
+
+    var lista_requisitos=""
+    result1.forEach(generarTablasRU); 
+    function generarTablasRU(value, index, array) { //Funcion que genera el codigo html con las tablas de requisitos
+        lista_requisitos = lista_requisitos + 
+        '<table style="margin-left: auto; margin-right: auto; border: 1px solid black; border-collapse: collapse; height: 206px;" width="529">'+"\n"+
+        '<tbody>'+"\n"+
+        '<tr>'+"\n"+
+        '<td style="width: 525px; text-align: left;">&nbsp;<strong style="text-align: left;">'+value.idRU+'</strong><strong style="text-align: left;"> - '+value.nombre+'</strong>'+"\n"+
+        '<p style="text-align: left;"><strong>Descripci&oacute;n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : </strong>'+value.descripcion+'</p>'+"\n"+
+        '<p style="text-align: left;"><strong>Fuente&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : </strong>'+value.fuente+'</p>'+"\n"+
+        '<p style="text-align: left;"><strong>Estabilidad&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : </strong>'+value.estabilidad+'</p>'+"\n"+
+        '<p style="text-align: left;"><strong>Tipo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : </strong>'+value.tiporeq+'</p>'+"\n"+
+        '</td>'+"\n"+
+        '</tr>'+"\n"+
+        '</tbody>'+"\n"+
+        '</table>'+"\n"+
+        '<p>&nbsp;</p>'+"\n"
+    }
     const today = new Date();
 return `
 <p style="text-align: left;"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Utalca.svg/300px-Utalca.svg.png" width="70" height="70" /><strong><br /></strong><strong>FACULTAD DE INGENIER&Iacute;A<br /></strong><strong>UNIVERSIDAD DE TALCA<br /></strong>DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION (DCC)&nbsp;&nbsp;</p>
@@ -67,55 +86,45 @@ return `
 <p>&nbsp;</p>
 <h1 style="text-align: center;">&Iacute;ndice</h1>
 <p>&nbsp;</p>
-<p style="text-align: center;"><strong>1&nbsp;&nbsp;&nbsp; Introducci&oacute;n............................................................................................................................. 1</strong></p>
+<p style="text-align: center;"><strong>1&nbsp;&nbsp;&nbsp; Introducci&oacute;n................................................................................................................................. 1</strong></p>
 <p style="text-align: center;">1.1&nbsp;&nbsp;&nbsp; Prop&oacute;sito del Sistema................................................................................................................ 1</p>
-<p style="text-align: center;">1.2&nbsp;&nbsp;&nbsp; Alcance del Proyecto................................................................................................................. 1</p>
-<p style="text-align: center;">1.3&nbsp;&nbsp;&nbsp; Contexto................................................................................................................................... 1</p>
+<p style="text-align: center;">1.2&nbsp;&nbsp;&nbsp; Alcance del Proyecto................................................................................................................ 1</p>
+<p style="text-align: center;">1.3&nbsp;&nbsp;&nbsp; Contexto.................................................................................................................................. 1</p>
 <p style="text-align: center;">1.4&nbsp;&nbsp;&nbsp; Referencias............................................................................................................................... 1</p>
-<p style="text-align: center;"><strong>2&nbsp;&nbsp;&nbsp; Descripci&oacute;n General............................................................................................................... 2</strong></p>
-<p style="text-align: center;">2.1&nbsp;&nbsp;&nbsp; Suposiciones y Dependencias..................................................................................................... 2</p>
-<p style="text-align: center;">2.2&nbsp;&nbsp;&nbsp; Restricciones Generales............................................................................................................. 2</p>
-<p style="text-align: center;"><strong>3&nbsp;&nbsp;&nbsp; Requisitos del Usuario........................................................................................................... 3</strong></p>
+<p style="text-align: center;"><strong>2&nbsp;&nbsp;&nbsp; Descripci&oacute;n General...................................................................................................................... 2</strong></p>
+<p style="text-align: center;">2.1&nbsp;&nbsp;&nbsp; Suposiciones y Dependencias......................................................................................................... 2</p>
+<p style="text-align: center;">2.2&nbsp;&nbsp;&nbsp; Restricciones Generales.................................................................................................................. 2</p>
+<p style="text-align: center;">2.3&nbsp;&nbsp;&nbsp; Usuarios........................................................................................................................................ 2</p>
+<p style="text-align: center;"><strong>3&nbsp;&nbsp;&nbsp; Requisitos del Usuario................................................................................................................... 3</strong></p>
 <div style="page-break-after: always;">&nbsp;</div>
 <p>&nbsp;</p>
 <h1><a name="_Toc353436287"></a>&nbsp; 1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Introducci&oacute;n</h1>
 <h2><a name="_Toc353436288"></a>&nbsp; 1.1&nbsp;&nbsp;&nbsp; Prop&oacute;sito del Sistema</h2>
-<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp; &nbsp;${result2[0].proposito}</p>
 <p>&nbsp;</p>
 <h2><a name="_Toc353436288"></a>&nbsp; 1.2&nbsp;&nbsp;&nbsp; Alcance del Proyecto</h2>
-<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp; &nbsp;${result2[0].alcance}</p>
 <p>&nbsp;</p>
 <h2><a name="_Toc353436288"></a>&nbsp; 1.3&nbsp;&nbsp;&nbsp; Contexto</h2>
-<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp; &nbsp;${result2[0].contexto}</p>
 <p>&nbsp;</p>
 <h2><a name="_Toc353436288"></a>&nbsp; 1.4&nbsp;&nbsp;&nbsp; Referencias</h2>
-<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp; &nbsp;${result2[0].referencias}</p>
 <div style="page-break-after: always;">&nbsp;</div>
 <p>&nbsp;</p>
 <h1><a name="_Toc353436287"></a>&nbsp; 2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Descripci&oacute;n general</h1>
 <h2><a name="_Toc353436288"></a>&nbsp; 2.1&nbsp;&nbsp;&nbsp; Suposiciones y Dependencias</h2>
-<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp; &nbsp;${result2[0].suposiciones}</p>
 <p>&nbsp;</p>
 <h2><a name="_Toc353436288"></a>&nbsp; 2.2&nbsp;&nbsp;&nbsp; Restricciones Generales</h2>
-<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp; &nbsp;${result2[0].restricciones}</p>
 <p>&nbsp;</p>
 <h2><a name="_Toc353436288"></a>&nbsp; 2.3&nbsp;&nbsp;&nbsp; Usuarios</h2>
-<p>&nbsp; &nbsp;${name}</p>
+<p>&nbsp; &nbsp;${result2[0].usuarios}</p>
 <div style="page-break-after: always;">&nbsp;</div>
 <p>&nbsp;</p>
 <h1><a name="_Toc353436287"></a>&nbsp; 3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Requisitos de Usuario</h1>
 <p>&nbsp;</p>
-<table style="margin-left: auto; margin-right: auto; border: 1px solid black; border-collapse: collapse; height: 206px;" width="529">
-<tbody>
-<tr>
-<td style="width: 525px; text-align: left;">&nbsp;<strong style="text-align: left;">RU0002</strong><strong style="text-align: left;"> - Validaci&oacute;n de los alumnos</strong>
-<p style="text-align: left;"><strong>Descripci&oacute;n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :</strong> Validaci&oacute;n del nombre de usuario, contrase&ntilde;a y carrera al momento de autentificarse en el sistema.</p>
-<p style="text-align: left;"><strong>Fuente&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : </strong>Cliente</p>
-<p style="text-align: left;"><strong>Estabilidad&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Intransable/intransable</strong></p>
-<p style="text-align: left;"><strong>Tipo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Funcional/restricci&oacute;n/calidad</strong></p>
-</td>
-</tr>
-</tbody>
-</table>
+${lista_requisitos}
 `;
 };
